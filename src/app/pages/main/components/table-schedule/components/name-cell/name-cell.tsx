783f225c1e15469080c2f0b358000785f9568a6d
Viktar Zanimonets <vzanimonets@gmail.com>
2020-09-10 21:49:32 +0300

    fix: implementation of ant design typography

--- src/app/pages/main/components/table-schedule/components/name-cell/name-cell.tsx
+++ src/app/pages/main/components/table-schedule/components/name-cell/name-cell.tsx
@@ -1,31 +1,25 @@
 import './name-cell.scss';
 
-import { EditOutlined } from '@ant-design/icons';
-import { Tooltip } from 'antd';
-import Text from 'antd/lib/typography/Text';
-import React, { useState, useEffect, FC } from 'react';
-
+import { Typography } from 'antd';
+import React, { useState, FC } from 'react';
 import { AppMode } from '../../../../../../models/app.models';
 
+const { Paragraph } = Typography;
 interface INameCell {
   name: string;
-  onEditClick: () => void;
   appMode: AppMode;
 }
 
-export const NameCell: FC<INameCell> = ({ name, onEditClick, appMode }) => {
-  const [userName, setName] = useState<string>();
-
-  useEffect(() => setName(name), [name]);
+export const NameCell: FC<INameCell> = ({ name, appMode }) => {
+  const [userName, setName] = useState<string>(name);
 
   return (
     <div className="name-cell_wrapper">
-      <Text className="name-cell_name">{userName}</Text>
-      {appMode === AppMode.mentor && (
-        <Tooltip title="Edit Name">
-          <EditOutlined className="name-cell_edit-icon" onClick={onEditClick} />
-        </Tooltip>
-      )}
+      {
+        <Paragraph ellipsis={true} editable={appMode === AppMode.mentor ? { onChange: setName } : false}>
+          {userName}
+        </Paragraph>
+      }
     </div>
   );
 };

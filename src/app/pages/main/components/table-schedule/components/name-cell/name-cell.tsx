441bbf69e7a3c4061932f20d6c89c9cb7c8ad9f5
Viktar Zanimonets <vzanimonets@gmail.com>
2020-09-11 12:04:19 +0300

    fix: implementation of code review issues

--- src/app/pages/main/components/table-schedule/components/name-cell/name-cell.tsx
+++ src/app/pages/main/components/table-schedule/components/name-cell/name-cell.tsx
@@ -1,22 +1,31 @@
 import './name-cell.scss';
 
 import { Typography } from 'antd';
-import React, { useState, FC } from 'react';
+import React, { useState, useEffect, FC, useCallback } from 'react';
 import { AppMode } from '../../../../../../models/app.models';
 
 const { Paragraph } = Typography;
 interface INameCell {
   name: string;
   appMode: AppMode;
+  onEditClick: () => void;
 }
 
-export const NameCell: FC<INameCell> = ({ name, appMode }) => {
+export const NameCell: FC<INameCell> = ({ name, appMode, onEditClick }) => {
   const [userName, setName] = useState<string>(name);
+  useEffect(() => setName(userName), [userName]);
 
+  const onChange = useCallback(
+    (value: React.SetStateAction<string>) => {
+      setName(value);
+      onEditClick();
+    },
+    [onEditClick],
+  );
   return (
     <div className="name-cell_wrapper">
       {
-        <Paragraph ellipsis={true} editable={appMode === AppMode.mentor ? { onChange: setName } : false}>
+        <Paragraph ellipsis={true} editable={appMode === AppMode.mentor ? { onChange: onChange } : false}>
           {userName}
         </Paragraph>
       }

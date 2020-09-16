4d4c055d38fcf6fb73af383860b32819229ce0c6
Viktar Zanimonets <vzanimonets@gmail.com>
2020-09-16 22:14:11 +0300

    feat: implement 'broadcast-url-cell' component for table

--- src/app/pages/main/components/table-schedule/components/broadcast-url-cell/broadcast-url-cell.tsx
+++ src/app/pages/main/components/table-schedule/components/broadcast-url-cell/broadcast-url-cell.tsx
@@ -1,31 +1,31 @@
-import './broadcast-url-cell.scss';
-
-import { EditOutlined } from '@ant-design/icons';
-import { Anchor, Tooltip } from 'antd';
 import React, { useState, useEffect, FC } from 'react';
 
 import { AppMode } from '../../../../../../models/app.models';
+import { Anchor, Tooltip } from 'antd';
+import './broadcast-url-cell.scss';
+
+import { EditOutlined } from '@ant-design/icons';
+
+const { Link } = Anchor;
 
 interface IBroadcastURL {
   link: string;
-  name: string;
   onEditClick: () => void;
   appMode: AppMode;
 }
 
 export const BroadcastURLCell: FC<IBroadcastURL> = ({ link, onEditClick, appMode }) => {
   const [URL, setUrl] = useState<string>();
-
   useEffect(() => setUrl(link), [link]);
 
   return (
     <div className="broadcast-url_wrapper">
       <Anchor affix={false}>
-        <Anchor.Link href={URL} title={URL} />
+        <Link href={URL} title={'Broadcast URL'} />
       </Anchor>
       {appMode === AppMode.mentor && (
-        <Tooltip title={URL}>
-          <EditOutlined className="broadcast-url-cell_edit-icon" onClick={onEditClick} />
+        <Tooltip title="Broadcast URL">
+          <EditOutlined className="broadcast-cell_edit-icon" onClick={onEditClick} />
         </Tooltip>
       )}
     </div>

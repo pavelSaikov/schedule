c7fa71bcc843b7cae05dc3befd5c11a10ed219c7
Viktar Zanimonets <vzanimonets@gmail.com>
2020-09-20 13:23:14 +0300

    feat: implement changing visibility of table colums

--- src/app/pages/main/components/table-schedule/components/table-content/table.tsx
+++ src/app/pages/main/components/table-schedule/components/table-content/table.tsx
@@ -1,14 +1,16 @@
-import { Table } from 'antd';
-import React, { useCallback, FC } from 'react';
+import './table.scss';
 
-import { AppMode, IEventCategory, TimeZone } from '../../../../../../models/app.models';
-import { ITableRowInfo } from '../../table-schedule.models';
+import { Button, Checkbox, Dropdown, Menu, Table } from 'antd';
+import React, { useCallback, useEffect, useMemo, useState, FC } from 'react';
+
+import { columnCategory, AppMode, IEventCategory, TimeZone } from '../../../../../../models/app.models';
 import { BroadcastURLCell } from '../broadcast-url-cell/broadcast-url-cell';
 import { DateCell } from '../date-cell/date-cell';
 import { EventCategoryCell, TimeCell } from '../index';
 import { NameCell } from '../name-cell/name-cell';
 import { OrganizerCell } from '../organizer-cell/organizer-cell';
 import { rowSelection } from './table.models';
+import { ITableRowInfo } from '../../table-schedule.models';
 
 interface ITableContent {
   rowsContent: ITableRowInfo[];
@@ -41,88 +43,171 @@ export const TableContent: FC<ITableContent> = ({
   onCommentEdit,
   onBroadcastUrlEdit,
 }) => {
-  const onNameEdit = useCallback((id: string, newValue: string) => {
-    console.log(`${id}:${newValue}`);
+  const [checkedColumns, setCheckedColumns] = useState([]);
+  const [isMenuVisible, changeMenuVisibility] = useState(false);
+  const [menuColumns, setMenuColumns] = useState([]);
+  const [filteredColumns, setFiltered] = useState([]);
+
+  const columns = useMemo(
+    () => [
+      {
+        title: 'Event type',
+        dataIndex: 'eventCategory',
+        render: (eventCategory: IEventCategory, record: ITableRowInfo) => ({
+          props: { width: '150px' },
+          children: (
+            <EventCategoryCell
+              appMode={appMode}
+              eventCategory={eventCategory}
+              onEditClick={() => onCategoryEdit(record.id)}
+            />
+          ),
+        }),
+      },
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        render: (text: string, record: ITableRowInfo) => ({
+          props: { width: '150px' },
+          children: (
+            <NameCell
+              name={text}
+              appMode={appMode}
+              onEditClick={(title: string) => onTitleEdit(record.id, title)}
+            />
+          ),
+        }),
+      },
+      {
+        title: 'Comment',
+        dataIndex: 'comment',
+        render: (text, record) => ({
+          props: {
+            width: '100px',
+          },
+          children: text,
+        }),
+      },
+      {
+        title: 'Date',
+        dataIndex: 'dateTime',
+        render: (text: string, record: ITableRowInfo) => ({
+          props: { width: '150px' },
+          children: (
+            <DateCell
+              appMode={appMode}
+              onEditClick={() => onDateEdit(record.id, record.eventCategory.categoryName)}
+              dateTime={text}
+              timeZone={timeZone}
+            />
+          ),
+        }),
+      },
+      {
+        title: 'Time',
+        dataIndex: 'dateTime',
+        visible: false,
+        render: (text: string, record: ITableRowInfo) => ({
+          props: { width: '150px' },
+          children: (
+            <TimeCell
+              appMode={appMode}
+              onEditClick={() => onTimeEdit(record.id, record.eventCategory.categoryName)}
+              dateTime={text}
+              timeZone={timeZone}
+            />
+          ),
+        }),
+      },
+      {
+        title: 'Organizer',
+        dataIndex: 'organizer',
+        render: (text: string, record: ITableRowInfo) => ({
+          props: { width: '200px' },
+          children: (
+            <OrganizerCell appMode={appMode} gitLink={text} onEditClick={() => onOrganizerEdit(record.id)} />
+          ),
+        }),
+      },
+      {
+        title: 'BroadcastUrl',
+        dataIndex: 'broadcastUrl',
+        render: (url: string, record: ITableRowInfo) => ({
+          props: { width: '150px' },
+          children: (
+            <BroadcastURLCell
+              appMode={appMode}
+              link={url}
+              onEditClick={() => onBroadcastUrlEdit(record.id)}
+            />
+          ),
+        }),
+      },
+    ],
+    [
+      appMode,
+      onBroadcastUrlEdit,
+      onCategoryEdit,
+      onDateEdit,
+      onOrganizerEdit,
+      onTimeEdit,
+      onTitleEdit,
+      timeZone,
+    ],
+  );
+
+  const filterColumns = useCallback(() => {
+    const filtered = columns.filter(el => {
+      return checkedColumns.indexOf(el.title) !== -1;
+    });
+    setFiltered(filtered);
+  }, [checkedColumns, columns, setFiltered]);
+
+  useEffect(() => {
+    const cols = [];
+    columns.map(column => cols.push(column.title));
+    setCheckedColumns(cols);
+    setMenuColumns(cols);
+    setFiltered(columns);
+  }, [columns]);
+
+  useEffect(() => filterColumns(), [checkedColumns, filterColumns]);
+
+  const onChangeRowCategories = useCallback((checkedValues: columnCategory[]) => {
+    setCheckedColumns(checkedValues);
   }, []);
 
-  const columns = [
-    {
-      title: 'Event type',
-      dataIndex: 'eventCategory',
-      className: 'hide',
-      render: (eventCategory: IEventCategory, record: ITableRowInfo) => (
-        <EventCategoryCell
-          appMode={appMode}
-          eventCategory={eventCategory}
-          onEditClick={() => onCategoryEdit(record.id)}
-        />
-      ),
-    },
-    {
-      title: 'Title',
-      dataIndex: 'title',
-      render: (text: string, record: ITableRowInfo) => (
-        <NameCell
-          name={text}
-          appMode={appMode}
-          onEditClick={(title: string) => onTitleEdit(record.id, title)}
-        />
-      ),
-    },
-    {
-      title: 'Comment',
-      dataIndex: 'comment',
-    },
-    {
-      title: 'Date',
-      dataIndex: 'dateTime',
-      render: (text: string, record: ITableRowInfo) => (
-        <DateCell
-          appMode={appMode}
-          onEditClick={() => onDateEdit(record.id, record.eventCategory.categoryName)}
-          dateTime={text}
-          timeZone={timeZone}
-        />
-      ),
-    },
-    {
-      title: 'Time',
-      dataIndex: 'dateTime',
-      visible: false,
-      render: (text: string, record: ITableRowInfo) => (
-        <TimeCell
-          appMode={appMode}
-          onEditClick={() => onTimeEdit(record.id, record.eventCategory.categoryName)}
-          dateTime={text}
-          timeZone={timeZone}
-        />
-      ),
-    },
-    {
-      title: 'Organizer',
-      dataIndex: 'organizer',
-      render: (text: string, record: ITableRowInfo) => (
-        <OrganizerCell appMode={appMode} gitLink={text} onEditClick={() => onOrganizerEdit(record.id)} />
-      ),
-    },
-    {
-      title: 'BroadcastUrl',
-      dataIndex: 'broadcastUrl',
-      render: (url: string, record: ITableRowInfo) => (
-        <BroadcastURLCell appMode={appMode} link={url} onEditClick={() => onBroadcastUrlEdit(record.id)} />
-      ),
-    },
-  ];
-
+  const handleVisibilityChange = useCallback((flag: boolean) => changeMenuVisibility(flag), []);
+  const visibilityMenu = useMemo(
+    () => (
+      <Menu>
+        <Menu.Item title="Columns">
+          <Checkbox.Group
+            onChange={onChangeRowCategories}
+            options={menuColumns}
+            value={checkedColumns}
+            className="table_checkbox-group"
+          />
+        </Menu.Item>
+      </Menu>
+    ),
+    [checkedColumns, menuColumns, onChangeRowCategories],
+  );
   return (
-    <Table
-      rowSelection={{
-        ...rowSelection,
-      }}
-      rowKey={(record: ITableRowInfo) => record.id + record.eventCategory.categoryName}
-      columns={columns}
-      dataSource={rowsContent}
-      pagination={false}
-    />
+    <>
+      <Dropdown overlay={visibilityMenu} onVisibleChange={handleVisibilityChange} visible={isMenuVisible}>
+        <Button>Show/Hide</Button>
+      </Dropdown>
+      <Table
+        rowKey={(record: ITableRowInfo) => record.id + record.eventCategory.categoryName}
+        rowSelection={{
+          ...rowSelection,
+        }}
+        columns={filteredColumns}
+        dataSource={rowsContent}
+        pagination={false}
+        className={'table-schedule'}
+      />
+    </>
   );
 };
